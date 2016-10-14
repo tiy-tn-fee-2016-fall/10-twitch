f15116ba1b6d51d6d402bd4df29d1b3bf8de450d
Derrick Bray <derrickbray@yahoo.com>
2016-10-14 13:27:33 -0500

    copied

--- app/exercise/reverse-list.js
+++ app/exercise/reverse-list.js
@@ -2,9 +2,8 @@ export default function reverseList(list, output) {
   const children = list.children;
 
   for (let i = children.length - 1; i >= 0; i--) {
-    list.cloneNode(true);
-    output = parseInt(children[i].innerHTML);
-  }
+    const copy = children[i].cloneNode(true);
 
-  return;
+    output.appendChild(copy);
+  }
 }
