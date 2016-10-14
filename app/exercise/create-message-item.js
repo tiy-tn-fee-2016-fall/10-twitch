50f4caa789abc4d8c63b82d3f48a24c67cec724b
Derrick Bray <derrickbray@yahoo.com>
2016-10-13 19:07:30 -0500

    trying to return obj

--- app/exercise/create-message-item.js
+++ app/exercise/create-message-item.js
@@ -1,5 +1,13 @@
 
 export default function (message) {
   const createMessageItem = document.createElement('li');
+
   return createMessageItem;
+  // const objOne = {
+  //   username: 'Ryan',
+  //   message: 'Do your homework',
+  // };
+  function createMessageItem(objOne, objTwo) {
+    return objOne{username, message};
+  }
 }
