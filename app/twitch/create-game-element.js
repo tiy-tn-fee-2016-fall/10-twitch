fb8ab7968d3dd5bc9c8ef76fe34fa5793f6e2f95
Derrick Bray <derrickbray@yahoo.com>
2016-10-14 14:27:24 -0500

    adding h4

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -4,7 +4,13 @@ export default function game(itemOne, itemTwo) {
 
   const itemName = document.createElement('h3');
   itemName.classList.add('game-item__name');
-  item.appendChild(itemName);
+  itemName.innerText = itemOne.name;
 
+  const itemPop = document.createElement('h4');
+  itemPop.classList.add('game-item__popularity');
+  itemPop.innerText = itemOne.popularity;
+
+  item.appendChild(itemName);
+  item.appendChild(itemPop);
   return item;
 }
