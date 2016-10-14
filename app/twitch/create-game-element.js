2eecf53090b3a37eba3b983a9d81f4d49ce24718
Derrick Bray <derrickbray@yahoo.com>
2016-10-14 14:15:51 -0500

    added div and h3

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -1,1 +1,10 @@
-export default null;
+export default function game(itemOne, itemTwo) {
+  const item = document.createElement('div');
+  item.classList.add('game-item');
+
+  const itemName = document.createElement('h3');
+  itemName.classList.add('game-item__name');
+  item.appendChild(itemName);
+
+  return item;
+}
