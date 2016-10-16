2626f40398d0719273cab168b3300665eca58390
Derrick Bray <derrickbray@yahoo.com>
2016-10-16 10:20:44 -0500

    comments

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -3,8 +3,8 @@ import createGameElement from './create-game-element';
 export default function showGamesInList(parentEl, [data]) {
   const gameItem = document.createElement('div');
 
-  // createGameElement(gameItem);
-  // gameItem.classList.add('game-item');
+  // createGameElement(gameItem);this breaks the test
+  // gameItem.classList.add('game-item');this doesnt break the test but dosent help either
 
 
   return gameItem;
