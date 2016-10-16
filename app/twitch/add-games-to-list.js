cf96771186c2fa1942d18cde969afb1cc6c152cc
Derrick Bray <derrickbray@yahoo.com>
2016-10-16 17:08:02 -0500

    test one not broke but still false

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -1,15 +1,10 @@
 import createGameElement from './create-game-element';
 
 export default function showGamesInList(parentEl, data) {
-  // console.log(data);
   // debugger;
   for (let i = 0; i < data.length; i++) {
-    const gameItem = data[i];
-    // parentEl = data[i];
-    createGameElement.appendChild(gameItem);
+    createGameElement(data[i]);
   }
+  return createGameElement;
 }
-  // return gameItem;
-// gameItem = createGameElement(parentEl);
-// gameItem.classList.add('game-item');this doesnt break the test but dosent help either
-// createGameElement(gameItem);this breaks the test, switched order--still breaks
+// console.log(createGameElement);
