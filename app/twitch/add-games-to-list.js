a8916e4943d8d6d0cbdf5be77814e55dbd995861
Derrick Bray <derrickbray@yahoo.com>
2016-10-16 10:51:17 -0500

    comments

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -3,8 +3,8 @@ import createGameElement from './create-game-element';
 export default function showGamesInList(parentEl, [data]) {
   const gameItem = document.createElement('div');
 
-  // createGameElement(gameItem);this breaks the test
   // gameItem.classList.add('game-item');this doesnt break the test but dosent help either
+  // createGameElement(gameItem);this breaks the test, switched order--still breaks
 
 
   return gameItem;
