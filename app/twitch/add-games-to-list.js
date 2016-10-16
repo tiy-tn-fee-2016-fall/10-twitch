5b04e2777c77fa3ea330e67af9ba94a5d37fa59c
Derrick Bray <derrickbray@yahoo.com>
2016-10-16 15:20:29 -0500

    for loop

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -1,8 +1,10 @@
 import createGameElement from './create-game-element';
 
-export default function showGamesInList(parentEl, [data]) {
-  const gameItem = createGameElement([data]).document.createElement('div');
-
+export default function showGamesInList(parentEl, data) {
+  const gameItem = createGameElement(data);
+  for (let i = 0; i < data.length; i++) {
+    data[i];
+  }
   parentEl.appendChild(gameItem);
 }
   // return gameItem;
