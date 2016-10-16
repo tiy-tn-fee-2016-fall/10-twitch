27e510ba1c17ec704e96623f156483391f2ac8fd
Derrick Bray <derrickbray@yahoo.com>
2016-10-16 10:07:10 -0500

    no headway with js

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -2,7 +2,9 @@ import createGameElement from './create-game-element';
 
 export default function showGamesInList(parentEl, [data]) {
   const gameItem = document.createElement('div');
-  gameItem.classList.add('game-item');
+
+  // createGameElement(gameItem);
+  // gameItem.classList.add('game-item');
 
 
   return gameItem;
