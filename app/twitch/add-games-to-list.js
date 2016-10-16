bdc0e6eb2d5df7b7310ba50271a4ca4fcae189f4
Derrick Bray <derrickbray@yahoo.com>
2016-10-16 11:26:05 -0500

    tried to copy one of the exersises

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -1,11 +1,11 @@
 import createGameElement from './create-game-element';
 
 export default function showGamesInList(parentEl, [data]) {
-  const gameItem = document.createElement('div');
+  const gameItem = createGameElement([data]).document.createElement('div');
 
-  // gameItem.classList.add('game-item');this doesnt break the test but dosent help either
-  // createGameElement(gameItem);this breaks the test, switched order--still breaks
-
-
-  return gameItem;
+  parentEl.appendChild(gameItem);
 }
+  // return gameItem;
+// gameItem = createGameElement(parentEl);
+// gameItem.classList.add('game-item');this doesnt break the test but dosent help either
+// createGameElement(gameItem);this breaks the test, switched order--still breaks
