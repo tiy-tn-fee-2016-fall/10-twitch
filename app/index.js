77debd04240baee9117185244e9635842a8f23bc
Derrick Bray <derrickbray@yahoo.com>
2016-10-17 14:48:31 -0500

    trying to linkjs to the page

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 import 'whatwg-fetch';
-import 'data.json';
-import 'twitch/add-games-to-list';
+import './data.json';
+import './add-games-to-list';
 
-
+const ul = document.querySelector('.results');
 // Look up a `ul` element with a class called `results`
+const results = document.createElement('div');
+results.classList.add('.results');
 // Add games to the results list based on the data from `data.json`
