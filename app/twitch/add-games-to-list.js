b9e56089cda729b5b1bdc9892c0b9e0009747e35
Derrick Bray <derrickbray@yahoo.com>
2016-10-17 14:00:07 -0500

    fin

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -1,12 +1,11 @@
 import createGameElement from './create-game-element';
 
 export default function showGamesInList(parentEl, data) {
-  // debugger;
+  parentEl.innerHTML = '';
+
   for (let i = 0; i < data.length; i++) {
     const gameItem = data[i];
-    createGameElement(gameItem);
-    // data.classList.add('gameItem');
-    return gameItem;
-    console.log(gameItem);
+    const el = createGameElement(gameItem);
+    parentEl.appendChild(el);
   }
 }
