080e123b54b2f1ab22170188a82a5a05b683045e
Derrick Bray <derrickbray@yahoo.com>
2016-10-18 13:28:57 -0500

    loop in index

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 import 'whatwg-fetch';
-import './data.json';
-import './twitch/add-games-to-list';
+import data from './data.json';
+import showGamesInList from './twitch/add-games-to-list';
 
-const ul = document.querySelector('.results');
 // Look up a `ul` element with a class called `results`
-const results = document.createElement('div');
-results.classList.add('.results');
 // Add games to the results list based on the data from `data.json`
+const ul = document.querySelector('.results');
+for (let i = 0; i < data.length; i++) {
+  const x = showGamesInList(data[i]);
+  ul.classList.appendChild(x);
+}
