b920d635ff72a8dd2c016851bbb3e055083ab75b
Derrick Bray <derrickbray@yahoo.com>
2016-10-23 11:42:29 -0500

    merged master

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import 'whatwg-fetch';
 import data from './data.json';
 import showGamesInList from './twitch/add-games-to-list';
-
 const ul = document.querySelector('.results');
 
 const x = showGamesInList(ul, data);
+// debugger;
